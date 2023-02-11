2c417d603d8fb4355e8837bfc32d1663d0351ccc
itsrusty <rusty3031@gmail.com>
2023-02-10 19:19:16 -0700

    deploy

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import { PORT } from "./config.js";
 import app from "./app.js";
+import express from "express";
+import employeesRoutes from "./routes/employees.routes.js"
+import employeesRoutesIndex from "./routes/index.routes.js";
+
+// app express
+const app = express();
+app.use(express.json())
+
+// import routes middlewares
+app.use("/api",employeesRoutes)
+app.use(employeesRoutesIndex)
 
 // config server
 app.listen(PORT)
