8f9be8560c9a493a069de84e6561436ca82b2994
itsrusty <rusty3031@gmail.com>
2023-02-10 19:52:12 -0700

    m

--- src/config.js
+++ src/config.js
@@ -6,4 +6,4 @@ export const DB_HOST = process.env.DB_HOST || "127.0.0.1";
 export const DB_USER = process.env.DB_USER || "root";
 export const DB_PASSWORD = process.env.DB_PASSWORD || "password";
 export const DB_DATABASE = process.env.DB_DATABASE || "test";
-export const DB_PORT = process.env.DB_PORT || 3306;
+export const DB_PORT = process.env.DB_PORT || 3307;
